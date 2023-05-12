4c928b73ca787e439fc5ed40391f1bf3faed0a00
nnaemekasama <80992877+nnaemekasama@users.noreply.github.com>
2023-05-12 00:51:24 -1100

    update

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
-
-import  cors from 'cors'
+import CORS from 'cors'
 
 const cors = require('cors')
 
