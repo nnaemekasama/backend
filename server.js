8537409fa549a57531f043ff58379332ca6e06cb
nnaemekasama <80992877+nnaemekasama@users.noreply.github.com>
2023-05-12 00:23:51 -1100

    cors update

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
 
+import  cors from 'cors'
+
+const cors = require('cors')
+
 
 
 dotenv.config()
@@ -33,6 +37,11 @@ app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 app.use('/api/upload', uploadRoutes)
+app.use(
+  cors({
+    origin: '*',
+  })
+)
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
