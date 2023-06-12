814201c91fd22e8b4da703eaddbe08afe6362dce
Nnaemeka Nnodim <mannynnodim@gmail.com>
2023-06-12 17:07:09 +0100

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,5 @@
-import asyncHandler from 'express-async-handler'
-import Product from '../models/productModel.js'
+import asyncHandler from "express-async-handler";
+import Product from "../models/productModel.js";
 
 // @desc    Fetch all products
 // @route   GET /api/products
@@ -9,7 +9,7 @@ export const getProducts = asyncHandler(async (req, res) => {
   const pageSize = 4;
   const page = Number(req.query.pageNumber) || 1;
 
-  console.log(req.query.keyword)
+  console.log(req.query.keyword);
   const keyword = req.query.keyword
     ? {
         name: {
@@ -19,40 +19,41 @@ export const getProducts = asyncHandler(async (req, res) => {
       }
     : {};
   const count = await Product.count({ ...keyword });
-  const products = await Product.find().skip(pageSize * (page - 1)).limit(pageSize)
+  const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
 
   res.json({ products, page, pages: Math.ceil(count / pageSize) });
 });
-
 
 // @desc   Fetch single product
 // @route  GET /api/products/:id
 // @access Public
 
 export const getProductById = asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id)
+  const product = await Product.findById(req.params.id);
   if (product) {
-    res.json(product)
+    res.json(product);
   } else {
-    res.status(404)
-    throw new Error('product not found')
+    res.status(404);
+    throw new Error("product not found");
   }
-})
+});
 
 // @desc   delete a product
 // @route  DELETE /api/products/:id
 // @access Private/Admin
 
 export const deleteProduct = asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id)
+  const product = await Product.findById(req.params.id);
   if (product) {
-    await product.remove()
-    res.json({ message: 'Product removed' })
+    await product.remove();
+    res.json({ message: "Product removed" });
   } else {
-    res.status(404)
-    throw new Error('product not found')
+    res.status(404);
+    throw new Error("product not found");
   }
-})
+});
 
 // @desc   create a product
 // @route  POST /api/products
@@ -60,20 +61,20 @@ export const deleteProduct = asyncHandler(async (req, res) => {
 
 export const createProduct = asyncHandler(async (req, res) => {
   const product = new Product({
-    name: 'Sample name',
+    name: "Sample name",
     price: 0,
     user: req.user._id,
-    image: '/images/sample.jpg',
-    brand: 'Sample brand',
-    category: 'Sample category',
+    image: "/images/sample.jpg",
+    brand: "Sample brand",
+    category: "Sample category",
     countInStock: 0,
     numReviews: 0,
-    description: 'Sample description',
-  })
+    description: "Sample description",
+  });
 
-  const createdProduct = await product.save()
-  res.status(201).json(createdProduct)
-})
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
 
 // @desc   update a product
 // @route  PUT /api/products/:id
@@ -81,61 +82,61 @@ export const createProduct = asyncHandler(async (req, res) => {
 
 export const updateProduct = asyncHandler(async (req, res) => {
   const { name, price, description, image, brand, category, countInStock } =
-    req.body
+    req.body;
 
-  const product = await Product.findById(req.params.id)
+  const product = await Product.findById(req.params.id);
 
   if (product) {
-    product.name = name
-    product.price = price
-    product.description = description
-    product.image = image
-    product.brand = brand
-    product.category = category
-    product.countInStock = countInStock
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
 
-    const updatedProduct = await product.save()
-    res.json(updatedProduct)
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
   } else {
-    res.status(404)
-    throw new Error('Product not found')
+    res.status(404);
+    throw new Error("Product not found");
   }
-})
+});
 
 // reviews
 export const reviewProduct = asyncHandler(async (req, res) => {
-  const { rating, comment } = req.body
+  const { rating, comment } = req.body;
 
-  const product = await Product.findById(req.params.id)
+  const product = await Product.findById(req.params.id);
 
   if (product) {
     const alreadyReviewed = product.reviews.find(
       (rev) => rev.user.toString() === req.user._id.toString()
-    )
+    );
     if (alreadyReviewed) {
-      res.status(400)
-      throw new Error('Product already reviewed')
+      res.status(400);
+      throw new Error("Product already reviewed");
     }
     const review = {
       name: req.user.name,
       rating: Number(rating),
       comment,
       user: req.user._id,
-    }
-    product.reviews.push(review)
-    product.numReviews = product.reviews.length
+    };
+    product.reviews.push(review);
+    product.numReviews = product.reviews.length;
     product.rating =
       product.reviews.reduce((acc, item) => item.rating + acc, 0) /
-      product.reviews.length
-    await product.save()
-    res.status(201).json({ message: 'review added' })
+      product.reviews.length;
+    await product.save();
+    res.status(201).json({ message: "review added" });
   } else {
-    res.status(404)
-    throw new Error('Product not found')
+    res.status(404);
+    throw new Error("Product not found");
   }
-})
+});
 
 export const getTopProucts = asyncHandler(async (req, res) => {
-  const products = await Product.find({}).sort({ rating: -1 }).limit(3)
-  res.json(products)
-})
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3);
+  res.json(products);
+});
